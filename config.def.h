8d7ed38790660be4904c15c1c62ba33eada7c537
Sung Kang <sk5156@gmail.com>
2026-01-25 14:26:06 -0600

    increase border size; revert bg back to cyan but keep border with seagreen

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -18,7 +18,7 @@ static const char col_sea_green[]        = "#15bfa3";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_sea_green, col_sea_green },
+	[SchemeSel]  = { col_gray4, col_cyan, col_sea_green },
 };
 
 /* tagging */
